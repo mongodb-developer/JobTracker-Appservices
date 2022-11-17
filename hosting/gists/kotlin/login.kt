5ca0d0598c2ab07193af1da8a12c5801dbbd919d
John Page <johnlpage@gmail.com>
2022-11-17 15:00:42 +0000

    Working on Demo Flow

--- hosting/gists/kotlin/login.kt
+++ hosting/gists/kotlin/login.kt
@@ -1,3 +1,14 @@
+import io.realm.kotlin.mongodb.App
+import io.realm.kotlin.mongodb.AppConfiguration
+import io.realm.kotlin.mongodb.Credentials
+
+
+ private val appService by lazy {
+        val appConfiguration =
+            AppConfiguration.Builder(appId = "jobtrackerrealmapp-mlapp").log(LogLevel.ALL).build()
+        App.create(appConfiguration)
+    }
+    
 suspend fun login(email: String, password: String): User {
         return appService.login(Credentials.emailPassword(email, password))
     }
