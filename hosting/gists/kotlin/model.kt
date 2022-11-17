edd815fd5b087ac2258bcf15c4a0016736dead1e
John Page <johnlpage@gmail.com>
2022-11-17 11:09:43 +0000

    Examples

---
+++ hosting/gists/kotlin/model.kt
@@ -0,0 +1,18 @@
+
+import io.realm.kotlin.types.ObjectId
+import io.realm.kotlin.types.RealmInstant
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.RealmUUID
+import io.realm.kotlin.types.annotations.PrimaryKey
+
+
+
+class Job : RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId.create()
+    var status: String? = ""
+    var desc: String = ""
+    var creationDate: RealmInstant? = null
+    var area: String = ""
+    var user: String? = ""
+}
